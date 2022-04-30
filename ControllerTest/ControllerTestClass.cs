bd861ea5692b84b2fa91838d226e6f8b145f5ae8
SIVANJANEYA N <sivanjineya7178@gmail.com>
2022-04-30 13:17:08 +0530

    firstcommit

---
+++ ControllerTest/ControllerTestClass.cs
@@ -0,0 +1,45 @@
+﻿using DevOpsSampleWebApp.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Web.Mvc;
+
+namespace ControllerTest
+{
+    [TestClass]
+    public class ControllerTestClass
+    {
+        [TestMethod]
+        public void Login()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Login() as ViewResult;
+            // Assert
+            Assert.AreEqual("Login",result.ViewName);
+
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ ControllerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ControllerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ControllerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16239d0f-1ec6-4e35-ac74-551b228bf85b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
